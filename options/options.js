0342b6375a6515061b6ff56a75b173d36b7ac0dd
Crowdbotics <team@crowdbotics.com>
2023-12-24 10:00:06 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fairy-presentation-45183",
+  url: "https://fairy-presentation-45183.botics.co",
+  api: "https://fairy-presentation-45183.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
